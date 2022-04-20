779db114f0165723d460b7121c34977e1fb77e21
newchk610@gmail.com <gprms@1208>
2022-04-20 16:38:53 +0900

    gitignore

--- src/datastructure/sort/QuickSort.java
+++ src/datastructure/sort/QuickSort.java
@@ -1,61 +1,77 @@
 package datastructure.sort;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class QuickSort {
     public static void main(String[] args) {
         // pivot 을 이용.
         // 투포인터 이용
-        int[] arr = new int[]{9,7,6,4,5,8,1,2,3,0};
-        quickSort(arr, 0, arr.length-1);
+        // pivot 보다 작은것은 left로 큰것은 right로 옮김.
+        // 투포인터 교차시 해당지점 -> pivot 위치
+        int[] arr = new int[]{9,3,2,1,4,5,6,7,0,8};
+//        List<Integer> list = Arrays.stream(arr).boxed().collect(Collectors.toList());
+//        List<Integer> result = quickSort(new ArrayList<Integer>(list));
+//        List<Integer> result = quickSort(list);
         System.out.println(Arrays.toString(arr));
+        quickSort(arr, 0, arr.length-1);
     }
 
     private static void quickSort(int[] arr, int low, int high) {
 
         if(low >= high) return;
-
-        int pivotIdx = partition(arr,low, high);
-
-        String[] x = new String[arr.length];
-        Arrays.fill(x, "");
-        System.out.println(low + "-" + pivotIdx + "-" + high);
-        x[low] = "l";
-        x[high] = "h";
-        if(!"".equals(x[pivotIdx])){
-            x[pivotIdx] += "p";
-        }else{
-            x[pivotIdx] = "p";
-        }
-        System.out.println(Arrays.toString(x));
-        quickSort(arr, low, pivotIdx-1);
-        quickSort(arr, pivotIdx+1, high);
-
-
+        int pivot = partition(arr, low, high);
+        quickSort(arr, low, pivot-1);
+        quickSort(arr, pivot+1, high);
     }
 
     private static int partition(int[] arr, int left, int right) {
-        int lo = left;
-        int hi = right;
-        int pivot = arr[left];
-
-        while(lo < hi){
-            while(arr[lo] <= pivot && lo < hi){
-                lo++;
+        int mid = (left+right)/2;
+        int pivot = arr[mid];
+        int st = left;
+        int end = right;
+        System.out.println(st + "~"+end);
+        while(left <= right){
+            while(arr[left] < pivot){
+                left++;
             }
-            while(arr[hi] > pivot && lo < hi){
-                hi--;
+            while(arr[right] > pivot){
+                right--;
             }
-            swap(arr, lo, hi);
+            if(left <= right){ // 교환후 인덱스 증가 하기  -> 왜?
+                swap(arr, left, right);
+                System.out.printf("SWAP %d and %d\n", left, right);
+                System.out.println(Arrays.toString(arr));
+                left++; right--;
+            }
         }
-        swap(arr, lo, left);
-
-        return lo;
+        return left;
     }
 
     private static void swap(int[] arr, int idx1, int idx2) {
         int tmp = arr[idx1];
         arr[idx1] = arr[idx2];
         arr[idx2] = tmp;
+    }
+
+    public static List<Integer> quickSort(List<Integer> list) {
+        System.out.println(list);
+        if (list.size() <= 1) return list;
+        int pivot = list.get(list.size() / 2);
+
+        List<Integer> lesserArr = new LinkedList<>();
+        List<Integer> equalArr = new LinkedList<>();
+        List<Integer> greaterArr = new LinkedList<>();
+
+        for (int num : list) {
+            if (num < pivot) lesserArr.add(num);
+            else if (num > pivot) greaterArr.add(num);
+            else equalArr.add(num);
+        }
+
+        return Stream.of(quickSort(lesserArr), equalArr, quickSort(greaterArr))
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
     }
 }
--- src/week4/Prog_기동과보설치.java
+++ src/week4/Prog_기동과보설치.java
@@ -1,4 +1,174 @@
 package week4;
 
+import common.Print;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Prog_기동과보설치 {
+    public static void main(String[] args) {
+        Solution60061 s = new Solution60061();
+        Print.answer(s.solution(5, new int[][]{{1, 0, 0, 1}, {1, 1, 1, 1}, {2, 1, 0, 1}, {2, 2, 1, 1}, {5, 0, 0, 1}, {5, 1, 0, 1}, {4, 2, 1, 1}, {3, 2, 1, 1}})
+                , new int[][]{{1, 0, 0}, {1, 1, 1}, {2, 1, 0}, {2, 2, 1}, {3, 2, 1}, {4, 2, 1}, {5, 0, 0}, {5, 1, 0}});
+        Print.answer(s.solution(5, new int[][]{{0, 0, 0, 1}, {2, 0, 0, 1}, {4, 0, 0, 1}, {0, 1, 1, 1}, {1, 1, 1, 1}, {2, 1, 1, 1}, {3, 1, 1, 1}, {2, 0, 0, 0}, {1, 1, 1, 0}, {2, 2, 0, 1}})
+                , new int[][]{{0, 0, 0}, {0, 1, 1}, {1, 1, 1}, {2, 1, 1}, {3, 1, 1}, {4, 0, 0}});
+    }
+}
+
+class Solution60061 {
+    static boolean[][] poll;
+    static boolean[][] bow;
+
+    public int[][] solution(int n, int[][] build_frame) {
+        int[][] answer = {};
+        poll = new boolean[n + 1][n + 1];
+        bow = new boolean[n + 1][n + 1];
+        //x, y는 기둥, 보를 설치 또는 삭제할 교차점의 좌표이며, [가로 좌표, 세로 좌표] 형태입니다.
+        //a는 설치 또는 삭제할 구조물의 종류를 나타내며, 0은 기둥, 1은 보를 나타냅니다.
+        //b는 구조물을 설치할 지, 혹은 삭제할 지를 나타내며 0은 삭제, 1은 설치를 나타냅니다.
+
+        for (int[] build : build_frame) {
+            int x = build[0];
+            int y = build[1];
+            int type = build[2];
+            int cmd = build[3];
+
+            if (type == 0) { // 기둥일때
+                if (cmd == 0) {// 삭제
+                    if (removeValidation(n, x, y, type)) {
+                        poll[y][x] = false;
+                    }
+                } else { // 설치
+                    if (validation(n, x, y, type)) {
+                        poll[y][x] = true;
+                    }
+                }
+            } else { // 보일때
+                if (cmd == 0) {// 삭제
+                    if (removeValidation(n, x, y, type)) {
+                        bow[y][x] = false;
+                    }
+                } else { // 설치
+                    if (validation(n, x, y, type)) {
+                        bow[y][x] = true;
+                    }
+                }
+            }
+        }
+
+        for (int i = poll.length - 1; i >= 0; i--) {
+            boolean[] line = poll[i];
+            System.out.println(Arrays.toString(line));
+        }
+
+
+        System.out.println("++");
+        for (int i = bow.length - 1; i >= 0; i--) {
+            boolean[] line = bow[i];
+            System.out.println(Arrays.toString(line));
+        }
+        List<int[]> list = new ArrayList<>();
+        for (int i = 0; i < poll.length; i++) {
+            for (int j = 0; j < poll[i].length; j++) {
+                if (poll[i][j]) {
+                    list.add(new int[]{j, i, 0});
+                }
+            }
+        }
+        for (int i = 0; i < bow.length; i++) {
+            for (int j = 0; j < bow[i].length; j++) {
+                if (bow[i][j]) {
+                    list.add(new int[]{j, i, 1});
+                }
+            }
+        }
+        Collections.sort(list, (o1, o2) -> o1[0] - o2[0] > 0 ? o1[0] - o2[0] : o1[1] - o2[1] > 0 ? o1[1] - o2[1] : o1[2] - o1[2]);
+        for (int[] item : list) {
+            System.out.print(Arrays.toString(item) + ", ");
+        }
+        return answer;
+    }
+
+    private boolean validation(int n, int x, int y, int type) {
+        boolean result = false;
+        if (type == 0) {
+            if (y == 0) {
+                return true;
+            } else {
+                if (poll[y - 1][x]) return true;
+            }
+            if (x == 0) {
+                if (bow[y][x]) {
+                    result = true;
+                }
+            } else if (x == n) {
+                if (bow[y][x - 1]) {
+                    result = true;
+                }
+            } else if (x > 0 && x < n) {
+                if ((bow[y][x - 1] && !bow[y][x]) || (!bow[y][x - 1] && bow[y][x])) {
+                    result = true;
+                }
+            }
+        } else {
+            if (x == 0) {
+                if (poll[y - 1][x]) {
+                    result = true;
+                }
+            } else if (x == n - 1) {
+                if (poll[y - 1][x + 1]) {
+                    result = true;
+                }
+            } else if (x > 0 && x < n) {
+                if (poll[y - 1][x + 1] || poll[y - 1][x]) {
+                    result = true;
+                } else if (bow[y][x - 1] && bow[y][x + 1]) {
+                    result = true;
+                }
+            }
+        }
+        return result;
+    }
+
+
+    private boolean removeValidation(int n, int x, int y, int type) {
+        boolean result = false;
+        if (type == 0) { //기둥
+            if (x - 1 >= 0 && x <= n) {
+                if (bow[y + 1][x - 1] && bow[y + 1][x - 1]) {
+                    poll[y][x] = true;
+                }
+            }
+        } else { // 보
+            if (x < n - 1 && x > 0) {
+                if (bow[y][x - 1] && bow[y][x + 1]) {
+                    if (poll[y - 1][x] && poll[y - 1][x + 1]) {
+                        result = true;
+                    }
+                } else if (bow[y][x - 1]) {
+                    if (poll[y - 1][x]) {
+                        result = true;
+                    }
+                } else if (bow[y][x + 1]) {
+                    if (poll[y - 1][x + 1]) {
+                        result = true;
+                    }
+                }
+            } else if (x == n - 1) {
+                if (poll[y][x]) {
+                    if (poll[y][x + 1]) poll[y][x + 1] = false;
+                    result = true;
+                }
+            } else if (x == 0) {
+                if (poll[y][x + 1]) {
+                    if (poll[y][x]) poll[y][x] = false;
+                    result = true;
+                }
+            }
+        }
+        return result;
+    }
+
 }
